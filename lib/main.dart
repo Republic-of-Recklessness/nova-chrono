9c8dc0f95f50256c7cabcdbf11e3210f850b7822
Aayush Borkar <aayushb14@gmail.com>
2021-04-10 00:06:26 +0530

    implement multiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:nova_chrono/models/chrono_list.dart';
+import 'package:nova_chrono/providers/chrono_list_provider.dart';
+import 'package:nova_chrono/services/firestore_service.dart';
 import 'package:nova_chrono/screens/login_page.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,26 +16,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: 'Modulus',
+    final firestoreService = FirestoreService();
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChronoListProvider()),
+        StreamProvider.value(
+          value: firestoreService.getLists(),
+          initialData: [
+            ChronoList(
+              listName: 'Sample List',
+              isEmpty: false,
+              listItems: {
+                'Monday': {
+                  'task1': 'lorem',
+                  'task2': 'ipsum',
+                }
+              },
+            )
+          ],
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Modulus',
+        ),
+        home: LoginPage(),
       ),
-      home: LoginPage(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:nova_chrono/components/list_card.dart';
+import 'package:nova_chrono/models/chrono_list.dart';
+import 'package:nova_chrono/providers/chrono_list_provider.dart';
 import 'package:nova_chrono/utilities/constants.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:nova_chrono/services/sign_in.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    final chronoLists = Provider.of<List<ChronoList>>(context);
+    final chronoListProvider = Provider.of<ChronoListProvider>(context);
 
     return Scaffold(
       backgroundColor: back_grey,
@@ -44,36 +46,19 @@ class HomePage extends StatelessWidget {
               height: 20.0,
               width: double.infinity,
             ),
-            FutureBuilder<QuerySnapshot>(
-              future: users.doc(userID).collection('lists').get(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Something is wrong.');
-                }
-                if (snapshot.connectionState == ConnectionState.done) {
-                  print('connection status is okay!');
-                  List<String> listNames = [];
-                  snapshot.data.docs.forEach((doc) {
-                    listNames.add(doc.id);
-                  });
-                  print(listNames);
-                  return Container(
-                    height: 250.0,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: listNames.length,
-                      itemBuilder: (BuildContext ctxt, int index) {
-                        return ListCard(
-                          colour: Colors.red,
-                          listName: listNames[index],
-                        );
-                      },
-                    ),
+            Container(
+              height: 250.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: chronoLists.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                  return ListCard(
+                    colour: Colors.red,
+                    listName: chronoLists[index].listName,
+                    listIndex: index,
                   );
-                }
-                return Text('loading');
-              },
+                },
+              ),
             ),
             SizedBox(
               height: 10.0,
@@ -83,38 +68,36 @@ class HomePage extends StatelessWidget {
               onPressed: () {
                 String newListTitle;
                 showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                          title: Text('New List'),
-                          content: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              TextField(
-                                autofocus: true,
-                                textAlign: TextAlign.center,
-                                onChanged: (newText) {
-                                  newListTitle = newText;
-                                },
-                              ),
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  users
-                                      .doc(userID)
-                                      .collection('lists')
-                                      .doc(newListTitle)
-                                      .set({'isEmpty': true})
-                                      .then((value) => print(
-                                          'Empty list $newListTitle created.'))
-                                      .catchError((error) => print(
-                                          'Failed to add list $newListTitle.'));
-                                  Navigator.of(context).pop();
-                                },
-                                icon: Icon(Icons.add),
-                                label: Text('Add List'),
-                              ),
-                            ],
-                          ),
-                        ));
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('New List'),
+                    content: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextField(
+                          autofocus: true,
+                          textAlign: TextAlign.center,
+                          onChanged: (newText) {
+                            newListTitle = newText;
+                          },
+                        ),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            ChronoList chronoList = ChronoList(
+                                listName: newListTitle,
+                                isEmpty: true,
+                                listItems: {});
+                            print(chronoLists);
+                            chronoListProvider.saveData();
+                            Navigator.of(context).pop();
+                          },
+                          icon: Icon(Icons.add),
+                          label: Text('Add List'),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
               },
               icon: Icon(Icons.add),
               label: Text(
